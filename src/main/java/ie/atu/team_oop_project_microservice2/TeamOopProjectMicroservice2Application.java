4fae75cc32ba338b2175007cb1ddbf0dca58298b
FinnjaBayer <g00470359@atu.ie>
2026-05-20 17:48:37 +0100

    added package folders

--- src/main/java/ie/atu/team_oop_project_microservice2/TeamOopProjectMicroservice2Application.java
+++ src/main/java/ie/atu/team_oop_project_microservice2/TeamOopProjectMicroservice2Application.java
@@ -1,4 +1,4 @@
-package com.example.team_oop_project_microservice2;
+package ie.atu.team_oop_project_microservice2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
