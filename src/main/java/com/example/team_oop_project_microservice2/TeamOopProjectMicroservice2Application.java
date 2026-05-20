13a6955c00345049d54def2c23853f7984032dd3
FinnjaBayer <g00470359@atu.ie>
2026-05-20 17:42:45 +0100

    created github for second microservice

---
+++ src/main/java/com/example/team_oop_project_microservice2/TeamOopProjectMicroservice2Application.java
@@ -0,0 +1,13 @@
+package com.example.team_oop_project_microservice2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamOopProjectMicroservice2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamOopProjectMicroservice2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/team_oop_project_microservice2/TeamOopProjectMicroservice2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.team_oop_project_microservice2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamOopProjectMicroservice2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
